91d82902accfa8806e86a48d37dc3fa1fbab57fc
VictorLins <victorcampos93@gmail.com>
2023-08-28 07:33:48 +0100

    Commit Test Exception

--- backend_testing.py
+++ backend_testing.py
@@ -4,6 +4,9 @@ import random
 user_id = random.randint(0,999) #Generate random User Id
 user_name = "User " + str(user_id) #Define User Name
 post_response = requests.post(f"http://127.0.0.1:5000/users/{user_id}", json={"user_name": f"{user_name}"})
+
+raise Exception("Test Exception")
+
 if post_response.ok:
     print("Response OK: " + str(post_response.json()))
 else:
